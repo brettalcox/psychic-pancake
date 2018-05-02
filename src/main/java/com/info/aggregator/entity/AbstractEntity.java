8b7e20facd7afaf8b9f1563ed352e81607dab329
Brett Alcox <brettalcox@bretts-mbp.attlocal.net>
2018-05-01 20:53:52 -0500

    abstractness

--- src/main/java/com/info/aggregator/entity/AbstractEntity.java
+++ src/main/java/com/info/aggregator/entity/AbstractEntity.java
@@ -3,5 +3,9 @@ package com.info.aggregator.entity;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-public class AbstractEntity {
+public abstract class AbstractEntity {
+    public static abstract class AbstractBuilder<T extends AbstractEntity, B extends AbstractBuilder<T, B>> {
+        public abstract B getThis();
+        public abstract T build();
+    }
 }
--- src/main/java/com/info/aggregator/model/AbstractModel.java
+++ src/main/java/com/info/aggregator/model/AbstractModel.java
@@ -1,4 +1,8 @@
 package com.info.aggregator.model;
 
-public class AbstractModel {
+public abstract class AbstractModel {
+    public static abstract class AbstractBuilder<T extends AbstractModel, B extends AbstractBuilder<T, B>> {
+        public abstract B getThis();
+        public abstract T build();
+    }
 }
